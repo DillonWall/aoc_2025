0a2b2800921cb97ce8834613c3affcc7ce03775b
Dillon Wall <dillonmwall@msn.com>
2025-12-07 19:25:37 +0800

    day6 done

--- day6/main.go
+++ day6/main.go
@@ -9,12 +9,13 @@ import (
 )
 
 type Problem struct {
-	Nums []int
-	Operator byte
+	Nums     []string
+	Operator string
 }
 
 var result int64 = 0
 var problems []Problem = make([]Problem, 0)
+var lines []string = make([]string, 0)
 
 func max(i, j int64) int64 {
 	if i > j {
@@ -40,53 +41,70 @@ func main() {
 		fmt.Printf("Error: %v\n", err)
 	}
 
-    calculateResults()
+    processNums()
 
 	fmt.Printf("Result is %v\n", result)
 }
 
 func processText(line string) {
-    // if line contains + or * then its a op line
-    operationLine := false
-    if strings.Contains(line, "+") || strings.Contains(line, "*") {
-        operationLine = true
-    }
-    firstLine := false
-    if problems == nil || len(problems) == 0 {
-        firstLine = true
-    }
-
-    parts := strings.Fields(line)
-    if !operationLine {
-        for i, part := range parts {
-            num, err := strconv.Atoi(part)
-            if err != nil {
-                fmt.Printf("Error: %v\n", err)
-            }
-            if firstLine {
-                problems = append(problems, Problem{Nums: []int{num}})
-            } else {
-                problems[i].Nums = append(problems[i].Nums, num)
-            }
-        }
-    } else {
-        for i, part := range parts {
-            problems[i].Operator = part[0]
-            res := 0
-            if problems[i].Operator == '+' {
-                for _, n := range problems[i].Nums {
-                    res += n
-                }
-            } else if problems[i].Operator == '*' {
-                res = 1
-                for _, n := range problems[i].Nums {
-                    res *= n
-                }
-            }
-            result += int64(res)
-        }
-    }
+	lines = append(lines, line)
 }
 
-func calculateResults() {
+func processNums() {
+	// add all nums and operators from lines to problems
+	colStrs := make([]string, 0)
+    operator := ""
+	for i := 0; i < len(lines[0]) + 5; i++ {
+		allSpace := true
+        colStrs = append(colStrs, "")
+		for j := 0; j < len(lines) - 1; j++ {
+            if i >= len(lines[j]) {
+                continue
+            }
+            newChar := lines[j][i]
+			colStrs[len(colStrs)-1] += string(newChar)
+			if newChar != ' ' {
+				allSpace = false
+			}
+		}
+        if i < len(lines[len(lines)-1]) {
+            operator += lines[len(lines)-1][i:i+1]
+        }
+
+        if allSpace && operator == "" {
+            break
+        }
+		if allSpace {
+            fmt.Printf("ColStrs: %+v with operator %s\n", colStrs, operator)
+			problems = append(problems, Problem{Nums: colStrs[:len(colStrs)-1], Operator: operator})
+            colStrs = make([]string, 0)
+            operator = ""
+		}
+	}
+
+    fmt.Printf("Problems: %+v\n", problems)
+
+	for _, problem := range problems {
+		op := strings.Trim(problem.Operator, " ")
+		res := 0
+		if op == "+" {
+			for _, n := range problem.Nums {
+				nInt, err := strconv.Atoi(strings.Trim(n, " "))
+				if err != nil {
+					fmt.Printf("Error converting string to int: %v\n", err)
+				}
+				res += nInt
+			}
+		} else if op == "*" {
+			res = 1
+			for _, n := range problem.Nums {
+				nInt, err := strconv.Atoi(strings.Trim(n, " "))
+				if err != nil {
+					fmt.Printf("Error converting string to int: %v\n", err)
+				}
+				res *= nInt
+			}
+		}
+		result += int64(res)
+	}
 }
